8510597c1a3c1f09477d0f98cd96e17d9d010550
amelnychuk <Alexander.Melnychuk@keystonett.com>
2017-04-04 15:34:45 +0300

    First folder and class for Alexander Melnychuk created

---
+++ homeworks/src/alexandermelnychuk/HomeWork.java
@@ -0,0 +1,10 @@
+package alexandermelnychuk;
+
+/**
+ * Created by alexml on 04.04.17.
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Home work");
+    }
+}
