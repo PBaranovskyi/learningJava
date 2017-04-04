8e7ac4ebe7e7aa96085b978eb06105c718b1feb2
pbaranovskyi <Pavlo.Baranovskyi@kstt.ua>
2017-04-04 13:46:23 +0300

    example commit of personal folder creation

---
+++ homeworks/src/pavlobaranovskyi/module1/example/HomeWork.java
@@ -0,0 +1,10 @@
+package pavlobaranovskyi.module1.example;
+
+/**
+ * Created by Pavlo Baranovskyi on 4/4/2017.
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Home work");
+    }
+}
