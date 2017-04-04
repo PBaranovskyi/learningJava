b8e19f0f3d4903ea406c9b4c7b956d493d983380
Rogachov <rognarock@gmail.com>
2017-04-04 16:07:41 +0300

    added Ilya Rogachov homework

---
+++ homeworks/src/ilyarogachov/Homework.java
@@ -0,0 +1,10 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 04.04.17.
+ */
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println("Home Work");
+    }
+}
