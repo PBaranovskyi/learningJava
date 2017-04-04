7ec77d254537d50579cc2f9a61877953ba90178b
pbaranovskyi <Pavlo.Baranovskyi@kstt.ua>
2017-04-04 13:54:44 +0300

    char type example

---
+++ module1/src/com/ksttlearning/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ksttlearning;
+
+/**
+ * Created by Pavlo Baranovskyi on 4/4/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ module1/src/com/ksttlearning/PrimitiveTypes.java
@@ -0,0 +1,19 @@
+package com.ksttlearning;
+
+/**
+ * Created by Pavlo Baranovskyi on 4/4/2017.
+ */
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        char charType1 = 'k';
+        char charType2 = 's';
+        char charType3 = 't';
+        char charType4 = 't';
+
+
+        System.out.println("" + charType1 + charType2 + charType3 + charType4);
+        System.out.println(charType1 + charType2 + charType3 + charType4);
+
+    }
+}
