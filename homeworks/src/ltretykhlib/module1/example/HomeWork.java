e561054e236cd32571224723cc742dabfbddd7af
Litre <tretykhlibl@gmail.com>
2017-04-04 16:13:45 +0300

    added ltretykhlib folder

---
+++ homeworks/src/ltretykhlib/module1/example/HomeWork.java
@@ -0,0 +1,11 @@
+package ltretykhlib.module1.example;
+
+/**
+ * Created by Tretykhlib on 04.04.2017.
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Home work");
+    }
+
+}
