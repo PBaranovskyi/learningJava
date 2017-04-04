9053f76180355fd485aaaf4a3119256b51cafca6
IrunkaQA <iryna.grabovska@gmail.com>
2017-04-04 15:26:35 +0300

    igrabovska Home Work adding

---
+++ homeworks/src/irynagrabovska/HomeWork.java
@@ -0,0 +1,10 @@
+package irynagrabovska;
+
+/**
+ * Created by whisper on 04-Apr-17.
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
