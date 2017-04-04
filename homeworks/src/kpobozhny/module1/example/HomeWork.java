80429488def10d50753afa73ffaffa53ed39ab68
Kostiantyn Pobozhni <k.pobozhny@gmail.com>
2017-04-04 15:46:59 +0300

    added kpobozhny.module1.example

---
+++ homeworks/src/kpobozhny/module1/example/HomeWork.java
@@ -0,0 +1,12 @@
+package kpobozhny.module1.example;
+
+/**
+ * Created by Kostiantyn Pobozhni on 04.04.2017.
+ */
+public class HomeWork {
+
+    public static void main(String[] args) {
+        System.out.println("Home work");
+    }
+
+}
