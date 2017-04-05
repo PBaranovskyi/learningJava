5ee6e16cbfc37d3e69259256997b8f725f0af578
Iryna Stoliarchuk <irina.stolyarchuk@gmail.com>
2017-04-05 15:41:14 +0300

    Test commit

---
+++ homeworks/src/irastolyarchuk/module1/example/HomeWork.java
@@ -0,0 +1,10 @@
+package irastolyarchuk.module1.example;
+
+/**
+ * Created by Iryna Stoliarchuk on 05.04.2017.
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Home work");
+    }
+}
