01e840188cfdc880c721fe45d09d6052b4eeace4
n1kropol <n1kropol@yandex.ua>
2017-04-05 10:28:57 +0300

    module01 HomeWork

---
+++ homeworks/src/ua/kstt/justymenko/module01/HomeWork.java
@@ -0,0 +1,10 @@
+package ua.kstt.justymenko.module01;
+
+/**
+ * Created by Ustymenko on 04/05/17.
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Module01 HomeWork");
+    }
+}
