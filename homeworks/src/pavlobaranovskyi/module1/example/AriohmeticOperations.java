08b01264ccf30ffaf25c5f0055720de6091f0240
Pavlo Baranovskyi <baranovskiy.p.p@gmail.com>
2017-04-05 19:15:37 +0300

    my first home work

---
+++ homeworks/src/pavlobaranovskyi/module1/example/AriohmeticOperations.java
@@ -0,0 +1,10 @@
+package pavlobaranovskyi.module1.example;
+
+/**
+ * Created by citsym on 05.04.17.
+ */
+public class AriohmeticOperations {
+    public static void main(String[] args) {
+        // some worke here
+    }
+}
