e4cd75c840b0195a04da09b8d8277cc8c2634d04
eugeniyd85 <eugeniyid@gmail.com>
2017-04-05 12:53:36 +0300

    - added folder eugeniyd

---
+++ homeworks/src/edyadechko/lesson1/homework1/HomeWork1.java
@@ -0,0 +1,12 @@
+package edyadechko.lesson1.homework1;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by eugeniyd on 05.04.2017.
+ */
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("Hello wold!");
+    }
+}
