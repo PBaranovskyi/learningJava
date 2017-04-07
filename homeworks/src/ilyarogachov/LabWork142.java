09c36eb8e8505d77a26070abd8de8d4a395bc733
rogachev <rognarock@gmail.com>
2017-04-07 17:21:42 +0300

    added 142  task

---
+++ homeworks/src/ilyarogachov/LabWork142.java
@@ -0,0 +1,17 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 07.04.17.
+ */
+public class LabWork142 {
+    public static void main(String[] args) {
+        int a = 200;
+        int f = 55;
+        int min=a-f;
+        int sum = a+f;
+        double divide = (double) a / (double) f;
+        int multiply =a*f;
+        int ost =  a%f;
+        System.out.println(min+"\n"+sum+"\n"+divide+"\n"+multiply+"\n"+ost);
+    }
+}
