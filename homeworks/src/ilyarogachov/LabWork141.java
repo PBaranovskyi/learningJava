f66b6a5d09147481b6cb713bdaa3d9792ada9038
rogachev <rognarock@gmail.com>
2017-04-07 16:44:44 +0300

    added first task

---
+++ homeworks/src/ilyarogachov/LabWork141.java
@@ -0,0 +1,19 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 07.04.17.
+ */
+public class LabWork141 {
+
+      public static void main(String[] args) {
+            int a=25;
+            double f=23.3;
+            float fl=42.5312412F;
+            char ch='T';
+            short sh=8000;
+            boolean bool=true;
+            long l=9223372036854L;
+
+            System.out.println(a+"\n"+f+"\n"+ch+"\n"+bool+"\n"+l+"\n"+fl+"\n"+sh);
+        }
+}
