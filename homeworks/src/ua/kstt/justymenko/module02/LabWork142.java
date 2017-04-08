03a243222632de540e3914249b839c359b9b3e57
n1kropol <n1kropol@yandex.ua>
2017-04-08 13:31:31 +0300

    made LW142

---
+++ homeworks/src/ua/kstt/justymenko/module02/LabWork142.java
@@ -0,0 +1,23 @@
+package ua.kstt.justymenko.module02;
+
+/**
+ * Created by n1kropol on 4/8/17.
+ */
+public class LabWork142 {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 3;
+
+        int sum = i + j;
+        int sub = i - j;
+        int mul = i * j;
+        int div = i / j;
+        int mod = i % j;
+
+        System.out.println("i + j = " + sum);
+        System.out.println("i - j = " + sub);
+        System.out.println("i * j = " + mul);
+        System.out.println("i / j = " + div);
+        System.out.println("i % j = " + mod);
+    }
+}
