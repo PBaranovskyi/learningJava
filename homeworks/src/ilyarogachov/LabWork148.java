8c23506b46e35b5fbd62ec0f50ef5172686d00ff
rogachev <rognarock@gmail.com>
2017-04-09 17:23:34 +0300

    added 148  task

---
+++ homeworks/src/ilyarogachov/LabWork148.java
@@ -0,0 +1,15 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 09.04.17.
+ */
+public class LabWork148 {
+    public static void main(String[] args) {
+        int a = 25;
+        double f=3.25;
+        char ch='F';
+        int m = 97;
+        System.out.println((int) f +"\n" + (double) a + "\n" + (long) f + "\n" + (int) ch + "\n" + (char) m);
+
+    }
+}
