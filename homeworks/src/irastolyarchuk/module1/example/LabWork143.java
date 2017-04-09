4b515c4ecf61606489907a202872a628195cc3f2
Iryna Stoliarchuk <irina.stolyarchuk@gmail.com>
2017-04-10 00:03:53 +0300

    New commit

---
+++ homeworks/src/irastolyarchuk/module1/example/LabWork143.java
@@ -0,0 +1,18 @@
+package irastolyarchuk.module1.example;
+
+/**
+ * Created by Iryna Stoliarchuk on 09.04.2017.
+ */
+public class LabWork143 {
+    public static void main(String[] args) {
+        boolean A=false;
+        boolean B=true;
+
+        System.out.println(A&B);
+        System.out.println(A|B);
+        System.out.println(A!=B);
+        System.out.println(A^B);
+
+
+    }
+    }
