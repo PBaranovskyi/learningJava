33d0fea60ed307ca3c0cf718c64c57f25328c6ef
rogachev <rognarock@gmail.com>
2017-04-09 17:07:06 +0300

    added 148  task

---
+++ homeworks/src/ilyarogachov/LabWork147.java
@@ -0,0 +1,15 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 09.04.17.
+ */
+public class LabWork147 {
+    public static void main(String[] args) {
+        int a = 25;
+        int b=10;
+        int c = (b >= 10) ? 25:0;
+        System.out.println(c);
+    }
+
+
+}
