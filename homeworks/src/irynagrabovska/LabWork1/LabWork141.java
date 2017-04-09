8643bdfb8d5d7225d08f3837f9f8c345899db709
IrunkaQA <iryna.grabovska@gmail.com>
2017-04-09 17:09:03 +0300

    igrabovska Home Work1 adding

--- homeworks/src/irynagrabovska/LabWork1/LabWork141.java
+++ homeworks/src/irynagrabovska/LabWork1/LabWork141.java
@@ -4,4 +4,18 @@ package irynagrabovska.LabWork1;
  * Created by whisper on 09-Apr-17.
  */
 public class LabWork141 {
+    public static void main(String[] args) {
+        byte figure1 = -128;
+        short figure2 = 3;
+        int figure3 = 8484;
+        long figure4 = 838383838;
+        float figure5 = 624.3f;
+        double figure6 = 7438742.2424;
+        char figure7 = 'h';
+        boolean figure8 = true;
+
+        System.out.println(figure1 + " " + figure2 + " " + figure3 + " " + figure4 + " " + figure5 + " " + figure6 + " " + figure7 + " " + figure8);
+
+
+    }
 }
---
+++ homeworks/src/irynagrabovska/LabWork1/LabWork148.java
@@ -0,0 +1,18 @@
+package irynagrabovska.LabWork1;
+
+/**
+ * Created by whisper on 09-Apr-17.
+ */
+public class LabWork148 {
+    public static void main(String[] args) {
+
+        int a = 45;
+        double b = (double) a;
+        System.out.println(b);
+
+        double c = 45.34;
+        float d = (float) c;
+        System.out.println(d);
+
+    }
+}
