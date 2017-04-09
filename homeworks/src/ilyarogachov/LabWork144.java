886da67b643b62eac47694b34f90e4f2eb4decbb
rogachev <rognarock@gmail.com>
2017-04-09 17:01:23 +0300

    added 144  task

---
+++ homeworks/src/ilyarogachov/LabWork144.java
@@ -0,0 +1,23 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 09.04.17.
+ */
+public class LabWork144 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b=5;
+
+        while(a>0){
+            System.out.println(a);
+            a--;
+        }
+
+        while(b>=5&&b<10){
+            b++;
+            System.out.println(b);
+        }
+
+    }
+}
