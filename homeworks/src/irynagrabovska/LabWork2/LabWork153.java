9cfbeab60164b6834e5bd23af8a7ec6f3c10af70
IrunkaQA <iryna.grabovska@gmail.com>
2017-04-11 17:37:08 +0300

    igrabovska HomeWork2 adding (I did what I could)

---
+++ homeworks/src/irynagrabovska/LabWork2/LabWork153.java
@@ -0,0 +1,15 @@
+package irynagrabovska.LabWork2;
+
+/**
+ * Created by whisper on 11-Apr-17.
+ */
+public class LabWork153 {
+     public static void main(String[] args){
+        for(int i = 1; i < 10; i++){
+            for(int j = 1; j < 10; j++){
+                System.out.print(j * i + "  ");
+            }
+            System.out.println("");
+        }
+    }
+}
