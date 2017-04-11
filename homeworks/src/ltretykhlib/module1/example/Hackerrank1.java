ce82d2135d263185f59004cfe0b0254b72406bd2
Litre <tretykhlibl@gmail.com>
2017-04-11 15:09:29 +0300

    added hackerrank tasks

---
+++ homeworks/src/ltretykhlib/module1/example/Hackerrank1.java
@@ -0,0 +1,20 @@
+package ltretykhlib.module1.example;
+
+import java.util.Scanner;
+
+/**
+ * Created by Tretykhlib on 11.04.2017.
+ */
+public class Hackerrank1 {
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        scan.close();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+
+    }
+}
