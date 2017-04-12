607d01ab610933f57c19b1227910918f769fdc55
Pavlo Baranovskyi <baranovskiy.p.p@gmail.com>
2017-04-12 17:31:31 +0300

    lesson 2

--- module1/src/com/ksttlearning/HelloWorld.java
+++ module1/src/com/ksttlearning/HelloWorld.java
@@ -5,7 +5,62 @@ package com.ksttlearning;
  */
 public class HelloWorld {
 
+    static int some;
+    static boolean bool;
+    String word;
+
     public static void main(String[] args) {
+        String word ;
+        int otherValue ;
+
+//        System.out.println(word);
+
         System.out.println("Hello world");
+
+        boolean ifIamStillYoung = true;
+        String condition = "I`m young";
+
+        doIHaveAPArty(ifIamStillYoung);
+        doIHaveAPArty(condition);
+
+
+
+        System.out.println(some);
+
     }
+
+    private static void doIHaveAPArty(boolean ifIamStillYoung) {
+
+        boolean notSick = true;
+        boolean inGoodMood = true;
+
+
+        if (ifIamStillYoung) {
+            int some = 0;
+            System.out.println("Have a party");
+        } else if (notSick) {
+            System.out.println("Have a party too");
+        } else if (inGoodMood) {
+            System.out.println("Definitely a party too");
+        } else
+            System.out.println("stay at home");
+    }
+
+
+    private static void doIHaveAPArty(String condition) {
+        switch (condition){
+            case "I`m not sick":
+                System.out.println("Have a party too");
+            case "I`m young":
+                System.out.println("Have a party");
+            case "In good mood":
+                System.out.println("Definitely a party too");
+            default:
+                System.out.println("stay at home");
+
+        }
+
+    }
+
 }
+
