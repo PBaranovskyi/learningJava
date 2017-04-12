1c1fccb72b018473ae15b79f320598efb2b5a4b9
rogachev <rognarock@gmail.com>
2017-04-12 14:27:55 +0300

    added 151  task

---
+++ homeworks/src/ilyarogachov/LabWork151.java
@@ -0,0 +1,21 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 12.04.17.
+ */
+public class LabWork151 {
+    public static void main(String[] args) {
+        String S = "";
+        int a = 0;
+            while(a<=8) {
+                a++;
+                for (int y = a; y >= 0; y--) {
+                    if (y != 0)
+                        S = S + y + " ";
+
+                }
+                System.out.println(S);
+                S = "";
+            }
+    }
+}
