aa4eb891f86ea46f785b5004b2d7f573f04f4276
rogachev <rognarock@gmail.com>
2017-04-13 16:57:37 +0300

    added 155  task

---
+++ homeworks/src/ilyarogachov/LabWork155.java
@@ -0,0 +1,18 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 13.04.17.
+ */
+public class LabWork155 {
+    public static void main(String[] args) {
+        int a=0;
+        for (int i=0; i<100; i++){
+            i++;
+            a=a+i;
+            System.out.println("The sum is: " + i);
+            System.out.println("The avg is: " + a/i);
+
+        }
+
+    }
+}
