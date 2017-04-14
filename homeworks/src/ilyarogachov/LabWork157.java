eb2a7a0a3fe30626b9fd877d623b15d406640365
rogachev <rognarock@gmail.com>
2017-04-14 13:45:38 +0300

    added 155  task

---
+++ homeworks/src/ilyarogachov/LabWork157.java
@@ -0,0 +1,13 @@
+package ilyarogachov;
+
+/**
+ * Created by rogachev on 14.04.17.
+ */
+public class LabWork157 {
+    public static void main(String[] args) {
+        double a = 25;
+        for(int i = 1; i<=a; i++){
+            double
+        }
+    }
+}
