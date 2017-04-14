1deb92fc1bc0855b3a855be212111248ed23e3c6
quitefire <b.handozhynski@gmail.com>
2017-04-14 15:59:00 +0300

    added hackerrank day 1 & 2 solutions

---
+++ homeworks/src/bogdanh/com/kstt/week1/hw1/HackerRankDay2.java
@@ -0,0 +1,19 @@
+package bogdanh.com.kstt.week1.hw1;
+
+import java.util.Scanner;
+
+public class HackerRankDay2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
