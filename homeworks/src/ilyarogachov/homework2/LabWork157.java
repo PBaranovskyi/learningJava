b69f046e3a2444e174b68924a8ec571978a0a8a0
rogachev <rognarock@gmail.com>
2017-04-14 14:43:56 +0300

    added 157  task done

--- homeworks/src/ilyarogachov/homework2/LabWork157.java
+++ homeworks/src/ilyarogachov/homework2/LabWork157.java
@@ -1,18 +1,22 @@
 package ilyarogachov.homework2;
 
+import java.util.Scanner;
+
 /**
  * Created by rogachev on 14.04.17.
  */
 public class LabWork157 {
     public static void main(String[] args) {
-        double a = 255;
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter sone number: ");
+        double a = scan.nextInt();
         int k=0;
         for(int i = 1; i<a; i++){
             double f = a%i;
             if (f==0){
                  k=k+i;
+                System.out.println(k);
             }
           }
-        System.out.println(k);
     }
 }
