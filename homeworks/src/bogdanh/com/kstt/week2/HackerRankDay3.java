895506b2d72f1b3dbe6cf5204a3fdd92adbb580d
quitefire <b.handozhynski@gmail.com>
2017-04-18 16:58:31 +0300

    fixed "java if else" task from hackerrank

--- homeworks/src/bogdanh/com/kstt/week2/HackerRankDay3.java
+++ homeworks/src/bogdanh/com/kstt/week2/HackerRankDay3.java
@@ -1,24 +1,24 @@
 package bogdanh.com.kstt.week2;
 
-/*If  is odd, print Weird
-  If  is even and in the inclusive range of  to , print Not Weird
-  If  is even and in the inclusive range of  to , print Weird
-  If  is even and greater than , print Not Weird*/
-
-
 import java.util.Scanner;
 
 public class HackerRankDay3 {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        int number = Lab152.readANumber(scanner);
-
-        System.out.println(
-                (number % 2 == 1 || (6 <= number && number <= 20) ? "Weird"
-                        : "Not Weird"));
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String ans = "";
+        if (n % 2 == 1) {
+            ans = "Weird";
+        } else {
+            if (n >= 2 && n <= 5) {
+                ans = "Not Weird";
+            } else if (n >= 6 && n <= 20) {
+                ans = "Weird";
+            } else {
+                ans = "Not Weird";
+            }
+        }
+        System.out.println(ans);
     }
-
 }
