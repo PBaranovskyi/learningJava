b1462a6f84ec8587a63bb99664b676273b22bdae
Litre <tretykhlibl@gmail.com>
2017-04-18 11:53:55 +0300

    change realisation of SumOfSquare class

--- homeworks/src/ltretykhlib/module1/example/SumOfSquare.java
+++ homeworks/src/ltretykhlib/module1/example/SumOfSquare.java
@@ -10,16 +10,16 @@ public class SumOfSquare {
     public static void main(String[] args) {
         int sum = 0;
         Scanner sc = new Scanner(System.in);
-       while (sc.hasNextInt()) {
-           int i = sc.nextInt();
-            String str= Integer.toString(i);
-         for(int l=0; l<str.length(); l++){
-           String ch= String.valueOf(str.charAt(l));
-             int each=Integer.parseInt(ch);
-             System.out.println(each);
-             sum+=each*each;
-         }
-        System.out.println(sum);
+        while (sc.hasNextInt()) {
+            int i = sc.nextInt();
+            String str = Integer.toString(i);
+            for (int l = 0; l < str.length(); l++) {
+                String ch = String.valueOf(str.charAt(l));
+                int each = Integer.parseInt(ch);
+                System.out.println(each);
+                sum += each * each;
+            }
+            System.out.println(sum);
         }
     }
 }
