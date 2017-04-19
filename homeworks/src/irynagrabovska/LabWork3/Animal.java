a936dea5b340d476a7ac35a54bede536f884f83c
IrunkaQA <iryna.grabovska@gmail.com>
2017-04-19 18:54:57 +0300

    igrabovska AnimalTask added + changes

--- homeworks/src/irynagrabovska/LabWork3/Animal.java
+++ homeworks/src/irynagrabovska/LabWork3/Animal.java
@@ -4,8 +4,8 @@ package irynagrabovska.LabWork3;
  * Created by whisper on 19-Apr-17.
  */
 public class Animal {
-    public String name;
-    public String foodname;
+    private String name;
+    private String foodname;
     public Animal(String name, String foodname){
         this.name = name;
         this.foodname = foodname;
