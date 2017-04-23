70bae473b03b202fc8b1479c7d12e106228c0bda
n1kropol <n1kropol@yandex.ua>
2017-04-23 07:38:46 +0300

    update task due don't use raw type, add 2 exapmles for myself

--- homeworks/src/ua/kstt/justymenko/day8maps/Solution.java
+++ homeworks/src/ua/kstt/justymenko/day8maps/Solution.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        Map dicMap = new HashMap<String, Integer>();
+        Map<String, Integer> dicMap = new HashMap<String, Integer>();
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
             String name = in.next();
