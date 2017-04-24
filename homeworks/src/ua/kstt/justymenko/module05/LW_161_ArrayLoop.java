e5f743abd47ec83622e2919398d5dbc6cb5d567f
n1kropol <n1kropol@yandex.ua>
2017-04-24 22:29:13 +0300

    LabWork 161 ArrayLoop

---
+++ homeworks/src/ua/kstt/justymenko/module05/LW_161_ArrayLoop.java
@@ -0,0 +1,17 @@
+package ua.kstt.justymenko.module05;
+
+/**
+ * Created by n1kropol on 4/24/17.
+ */
+public class LW_161_ArrayLoop {
+    public static void main(String[] args) {
+        int[] arr = new int[30];
+        int counter = 0;
+        for (int i = 0; i < arr.length / 2; i++) {
+            counter += 2;
+            arr[i] = counter;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
