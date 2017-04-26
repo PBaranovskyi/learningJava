c25225be3787ba8bb58dbd24a56fa72a31054190
Ilya <rognarock@gmail.com>
2017-04-26 12:29:22 +0300

    homework3

--- homeworks/src/ilyarogachov/homework3converter/Mainmeth.java
+++ homeworks/src/ilyarogachov/homework3converter/Mainmeth.java
@@ -7,7 +7,7 @@ public class Mainmeth {
     public static void main(String[] args) {
         Calculate c = new Calculate();
         double usd = c.hrivnausd("Hrivna", 5400.0);
-        System.out.println(usd);
+        System.out.println("Summa USD = " + usd);
     }
 
 
