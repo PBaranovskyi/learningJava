1e9baee84f9aea148ac2823c846d6fe51ef7890c
kpobozhny <k.pobozhny@gmail.com>
2017-04-29 17:59:50 +0300

    kpobozhny. changed logic in the CurrencyConverter. added new Triangle2 class.

--- homeworks/src/kpobozhny/homework/hw_04/currencyconverter/CurrencyConverter.java
+++ homeworks/src/kpobozhny/homework/hw_04/currencyconverter/CurrencyConverter.java
@@ -23,15 +23,15 @@ public class CurrencyConverter {
 
 
     public double convert(double amount) {
-        double converted = new BigDecimal(amount * rate).setScale(2, RoundingMode.UP).doubleValue();
+        double converted = new BigDecimal(amount / rate).setScale(2, RoundingMode.UP).doubleValue();
         return converted;
         //return Math.round(amount*rate * Math.pow(10, 2)) / Math.pow(10, 2);
 
     }
 
     public static void main(String[] args) {
-        CurrencyConverter currencyConverter = new CurrencyConverter(0.037);
-        double amountOfGryvna = 2560.75;
+        CurrencyConverter currencyConverter = new CurrencyConverter(26.68);
+        double amountOfGryvna = 2668;
         System.out.println("For the " + amountOfGryvna + " UAH you can get " + currencyConverter.convert(amountOfGryvna) + " USD");
     }
 }
