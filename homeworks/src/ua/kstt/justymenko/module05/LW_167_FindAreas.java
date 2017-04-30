152d477d4c37dc7806548679b48bd0fdd9eb7a84
n1kropol <n1kropol@yandex.ua>
2017-04-30 12:37:49 +0300

    implement LW 167 Find Areas output [2, 2, 3, 1, 2] [1, 1, 4, 1, 4] [5, 5, 4, 1, 5] [1, 1, 1, 1, 5] Line 1: [3] Line 2: [0,1,3] Line 3: [3] Line 4: [0-3]

--- homeworks/src/ua/kstt/justymenko/module05/LW_167_FindAreas.java
+++ homeworks/src/ua/kstt/justymenko/module05/LW_167_FindAreas.java
@@ -1,5 +1,8 @@
 package ua.kstt.justymenko.module05;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Created by n1kropol on 4/25/17.
  */
@@ -8,75 +11,51 @@ public class LW_167_FindAreas {
         int iDimension = 4;
         int jDimension = 5;
         int[][] matrix = new int[iDimension][jDimension];
-//        Random random = new Random();
-//        for (int i = 0; i < iDimension; i++) {
-//            for (int j = 0; j < jDimension; j++) {
-//                matrix[i][j] = random.nextInt(jDimension) + 1;
-//            }
-//        }
-//
-//        for (int i = 0; i < matrix.length; i++) {
-//            System.out.println(Arrays.toString(matrix[i]));
-//        }
-
-        int[] matrix1 = {1, 1, 1, 0, 1};
-        int[] matrix2 = {1, 0, 1, 1, 1};
-        int[] matrix3 = {1, 0, 1, 0, 1};
-        int[] matrix4 = {0, 1, 1, 1, 1};
-
-        matrix[0] = matrix1;
-        matrix[1] = matrix2;
-        matrix[2] = matrix3;
-        matrix[3] = matrix4;
-
-
-        StringBuilder builder = new StringBuilder();
         int finder = 1;
+        Random random = new Random();
+        for (int i = 0; i < iDimension; i++) {
+            for (int j = 0; j < jDimension; j++) {
+                matrix[i][j] = random.nextInt(jDimension) + 1;
+            }
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        StringBuilder builder = new StringBuilder();
         for (int i = 0; i < matrix.length; i++) {
-
             String prefix = "";
             int begin = 0;
             int end = 0;
             boolean isBeginSet = false;
-            boolean isContinueValue = false;
-
-            for (int j = 0; j < matrix.length - 2; j++) {
-
-                if (matrix[i][j] == finder && matrix[i][j + 1] == finder && matrix[i][j + 2] == finder){
-                    isContinueValue = true;
-                    if (!isBeginSet){
+            boolean isRepeated = false;
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (j + 2 < matrix[i].length && matrix[i][j] == finder && matrix[i][j + 1] == finder && matrix[i][j + 2] == finder) {
+                    isRepeated = true;
+                    if (!isBeginSet) {
                         isBeginSet = true;
                         begin = j;
                     }
                     end = j + 2;
-                    continue;
-                }
-                else{
-                    if (matrix.length - 2 != j){
-                        if (isContinueValue){
-                            builder.append(begin + "-" + end);
-                        }
+                    if (j + 2 != matrix[i].length - 1)
                         continue;
-                    }
-                    if (isContinueValue){
-                        builder.append(begin + "-" + end);
-                    }
-
-
+                }
+                if (isRepeated) {
+                    builder.append(prefix);
+                    builder.append(begin + "-" + end);
+                    prefix = ",";
+                    isRepeated = false;
+                    j = end + 1;
+                }
+                if (j < matrix[i].length && matrix[i][j] == finder) {
                     builder.append(prefix);
                     prefix = ",";
                     builder.append(j);
                 }
+                if (j < matrix[i].length)
+                    continue;
             }
-
-
             i++;
             System.out.println("Line " + i + ": [" + builder + "]");
             i--;
             builder.setLength(0);
         }
-
-
-
     }
 }
