cc2a6d567087848f2f2db7255657fe1d7739cf3a
n1kropol <n1kropol@yandex.ua>
2017-05-01 07:53:13 +0300

    add package and Solution class

---
+++ homeworks/src/ua/kstt/justymenko/day11/Solution.java
@@ -0,0 +1,36 @@
+package ua.kstt.justymenko.day11;
+
+import java.util.Scanner;
+
+/**
+ * Created by n1kropol on 5/1/17.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        int[] result = new int[16];
+        int k = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                result[k] = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                        arr[i + 1][j + 1] +
+                        arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                k++;
+            }
+        }
+
+        int max = result[0];
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] > max)
+                max = result[i];
+        }
+        System.out.println(max);
+    }
+}
