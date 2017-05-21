a77728d699b0238c122f69ecba20c03cb18de861
n1kropol <n1kropol@yandex.ua>
2017-05-21 12:48:52 +0300

    3-rd task implemented

--- homeworks/src/ua/kstt/justymenko/module07/MyMath.java
+++ homeworks/src/ua/kstt/justymenko/module07/MyMath.java
@@ -6,10 +6,13 @@ import java.util.Arrays;
  * Created by n1kropol on 5/21/17.
  */
 public class MyMath {
+    final static double PI = 3.14;
+
     public static void main(String[] args) {
         int[] array = {0, 1, -1, 3, 5};
         System.out.println("Max: " + findMax(array));
         System.out.println("Min: " + findMin(array));
+        System.out.println("Radius 4, square = " + areaOfCircle(4));
     }
 
     public static int findMax(int[] array){
@@ -18,5 +21,10 @@ public class MyMath {
 
     public static int findMin(int[] array){
         return Arrays.stream(array).max().getAsInt();
+    }
+
+    public static double areaOfCircle(double radius){
+
+        return PI * Math.pow(radius, 2);
     }
 }
