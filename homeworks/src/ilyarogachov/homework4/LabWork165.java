79ed07f512f69d7333095465859a8339ad688ac4
rogachev <rognarock@gmail.com>
2017-05-22 12:33:16 +0300

    165 task

--- homeworks/src/ilyarogachov/homework4/LabWork165.java
+++ homeworks/src/ilyarogachov/homework4/LabWork165.java
@@ -36,6 +36,7 @@ public class LabWork165 {
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 System.out.print(matrix[row][col] + "\t");
+
             }
             System.out.println();
         }
