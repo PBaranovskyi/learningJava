1cbb43c5e8168056da8db58d7eda73728650be8c
rogachev <rognarock@gmail.com>
2017-05-22 12:56:28 +0300

    homework5/1

---
+++ homeworks/src/ilyarogachov/homework5/LabWork241.java
@@ -0,0 +1,12 @@
+package ilyarogachov.homework5;
+
+/**
+ * Created by rogachev on 22.05.17.
+ */
+public class LabWork241 {
+    public static void main(String[] args) {
+        int[] m = new int[] { 10, 21, 5, 22, 9, 29, 25, 22, 11, 14, 8, 14 };
+        System.out.println(MyMath.findMax(m));
+        System.out.println(MyMath.findMin(m));
+    }
+}
