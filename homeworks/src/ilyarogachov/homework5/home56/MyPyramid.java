87ad17dc525e5b8010c4724ab0166b8f61b8fdb9
rogachev <rognarock@gmail.com>
2017-05-24 10:17:34 +0300

    homework5/6

--- homeworks/src/ilyarogachov/homework5/home56/MyPyramid.java
+++ homeworks/src/ilyarogachov/homework5/home56/MyPyramid.java
@@ -8,7 +8,6 @@ public class MyPyramid {
     public static void printPyramid(int p){
         int noOfRows = p;
         int rowCount = 1;
-        System.out.println("Here Is Your Pyramid");
         //Implementing the logic
         for (int i = noOfRows; i > 0; i--) {
             for (int j = 1; j <= i * 2; j++) {
