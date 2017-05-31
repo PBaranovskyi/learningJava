042a1763ecdd23c0310d5d3134fd07f84018e7d9
Oleksander Krasilnikov <Oleksander.Krasilnikov@keystonett.com>
2017-05-31 18:40:34 +0300

    homework8 1-5 tasks

---
+++ homeworks/src/oleksanderkrasilnikov/lesson6/Square.java
@@ -0,0 +1,13 @@
+package oleksanderkrasilnikov.lesson6;
+
+/**
+ * Created by Krasilnikov on 31.05.2017.
+ */
+public class Square extends Rectangle {
+
+    public Square(int sideA) {
+        super(sideA, sideA);
+    }
+
+
+}
